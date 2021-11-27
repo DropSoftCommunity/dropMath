9c5b9cfbb96d0a6e16b99853bba565efd38ad320
kxrdelis <jannikordelis@gmail.com>
2021-11-28 00:29:35 +0100

    Vector3 ~ sqiared_length() added Vector3 ~ length() added

--- header/dropMath.hpp
+++ header/dropMath.hpp
@@ -21,6 +21,18 @@ namespace dropMath{
 	
     class Vector3{
         float x, y, z;
+
+    public:
+        Vector3(float x=0, float y=0, float z=0): x{x}, y{y}, z{z}{
+        }
+
+        auto squared_length() -> float {
+            return x*x + y*y + z*z;
+        }
+
+        auto length() -> float {
+            return sqrt(this->squared_length());
+        }
     };
 }
 
