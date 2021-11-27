72100353862b827e9f72b5b49a29f00d62bc14a6
Erarnitox <erarnitox@gmail.com>
2021-11-28 00:21:32 +0100

    Vector2 length

--- header/dropMath.hpp
+++ header/dropMath.hpp
@@ -1,9 +1,24 @@
 #pragma once
 
+#include <math.h>
+
 namespace dropMath{
     class Vector2{
         float x, y;
+				
+		public:
+			Vector2(float x=0, float y=0): x{x}, y{y}{
+			}
+
+			auto squared_length() -> float {
+				return x*x + y+y; 
+			}
+
+			auto length() -> float {
+				return sqrt(this->squared_length());
+			}
     };
+	
     class Vector3{
         float x, y, z;
     };
