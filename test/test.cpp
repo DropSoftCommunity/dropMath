b4b9b330f13a808e604ec0fae78f2189fbfffa9b
Erarnitox <erarnitox@gmail.com>
2021-11-28 04:15:48 +0100

    simple test

--- test/test.cpp
+++ test/test.cpp
@@ -2,5 +2,12 @@
 
 
 int main(){
-	auto vec2 = drop::math::Vector2(1.f, 2.f);
+	using Vector2 = drop::math::Vector2;
+	auto v1 = Vector2(1.f, 2.f);
+	auto v2 = Vector2::up();
+
+	auto len1 = v1.length();
+	auto len2 = v2.length();
+
+	auto v3 = v1 + v2;
 }
