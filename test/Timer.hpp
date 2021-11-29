8f50ecf52850a5d2be35699b6222312a8af945d7
Erarnitox <erarnitox@gmail.com>
2021-11-29 02:59:54 +0100

    added Timer header/class for easy performance tracking of tests

---
+++ test/Timer.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <iostream>
+
+class Timer{
+	std::string name;
+	std::chrono::time_point<std::chrono::high_resolution_clock> start;
+	
+	public:
+		Timer(std::string&& name): name{name}{
+			std::cout << "Test: " << name << " started..." << std::endl;
+			start = std::chrono::high_resolution_clock::now();
+		}
+
+		~Timer(){
+			auto end = std::chrono::high_resolution_clock::now();
+
+			auto start_mic = std::chrono::time_point_cast<std::chrono::microseconds>(this->start).time_since_epoch();
+			auto end_mic = std::chrono::time_point_cast<std::chrono::microseconds>(end).time_since_epoch();	
+			
+			auto duration_mic = (end_mic - start_mic).count();
+			auto duration_ms = duration_mic * 0.001;
+
+			std::cout << "Test: " << name << " ended!\n"
+			<< "Duration: " << duration_ms  << "ms (" 
+			<< duration_mic << "mic)\n"
+			<< "----------------------------------------" << std::endl;
+		}
+};
--- test/Vector2_tests.hpp
+++ test/Vector2_tests.hpp
@@ -1,6 +1,7 @@
 #include "../header/dropMath.hpp"
 #include <cassert>
 #include <iostream>
+#include "Timer.hpp"
 
 bool Vector2_test(){
 	using Vector2 = drop::math::Vector2;
@@ -14,20 +15,23 @@ bool Vector2_test(){
 	 *    /        /
 	 *   D--------C
 	 */
-	auto A{ Vector2(1.f, 1.f) };	
-	auto B{ Vector2(5.f, 1.f) };	
-	auto C{ Vector2(4.f, 0.f) };	
-	auto D{ Vector2(0.f, 0.f) };	
+	{
+		auto t{ Timer("Pralelogram") };
 
-	//diagonals:
-	auto dia1{ D.to(B) };
-	auto dia2{ A.to(C) };
+		auto A{ Vector2(1.f, 1.f) };	
+		auto B{ Vector2(5.f, 1.f) };	
+		auto C{ Vector2(4.f, 0.f) };	
+		auto D{ Vector2(0.f, 0.f) };	
 
-	std::cout << "Diagonal 1:\t" << dia1 << std::endl;
-	std::cout << "Diagonal 2:\t" << dia2 << std::endl;
+		//diagonals:
+		auto dia1{ D.to(B) };
+		auto dia2{ A.to(C) };
 
-	assert(D+dia1*0.5f == A+dia2*0.5f);
-	if(D+0.5f*dia1 != A+0.5f*dia2) return false;
+		std::cout << "Diagonal 1:\t" << dia1 << std::endl;
+		std::cout << "Diagonal 2:\t" << dia2 << std::endl;
 
+		assert(D+dia1*0.5f == A+dia2*0.5f);
+		if(D+0.5f*dia1 != A+0.5f*dia2) return false;
+	}
 	return true;
 }
