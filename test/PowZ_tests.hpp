59e1288d869ab60d7d7c4dda8e4a4191360af4a6
Kxrdelis <66371779+Kxrdelis@users.noreply.github.com>
2021-12-06 22:29:10 +0100

    powZ tests.

--- test/PowZ_tests.hpp
+++ test/PowZ_tests.hpp
@@ -4,8 +4,8 @@ bool PowZ_tests(){
 	using drop::math::powZ;
     auto t{ Timer("PowZ ultimative test") };
 
-    for(int i{}; i<1000;++i)
-	std::cout << powZ(i,5) << std::endl;
+    for(int i{}; i<10000;++i)
+	std::cout << powZ(i,2) << std::endl;
 
 
 
