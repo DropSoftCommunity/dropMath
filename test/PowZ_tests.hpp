bafa69cfa47c3f8988708fef8cfff021487a4b1b
Kxrdelis <66371779+Kxrdelis@users.noreply.github.com>
2021-12-07 18:08:50 +0100

    powZ edit

--- test/PowZ_tests.hpp
+++ test/PowZ_tests.hpp
@@ -13,7 +13,7 @@ bool PowZ_tests(){
     auto t{ Timer("PowZ ultimative test") };
 
     for(int i{0}; i<10000;++i)
-	std::cout << powZ(rand() % 101-1,rand() % 101-1) << std::endl;
+	std::cout << powZ(rand() % 10001,rand() % 10100) << std::endl;
 	}
 
 
